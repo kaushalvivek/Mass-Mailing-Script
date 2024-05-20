5fe89340812637f7fe30bf50b4d908eeb8c5895c
= <=>
2024-05-20 15:08:58 +0530

    test mode

--- send_mail.py
+++ send_mail.py
@@ -5,6 +5,7 @@ import yaml
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from tqdm import tqdm
+import random
 
 with open('config.yaml', 'r') as config_file:
     config = yaml.safe_load(config_file)
@@ -18,10 +19,10 @@ parser = argparse.ArgumentParser(description='Process email details.')
 parser.add_argument('--subject', required=True, help='Subject of the email')
 parser.add_argument('--template', required=True, help='Path to the email template file')
 parser.add_argument('--csv', required=True, help='Path to the CSV file with recipient data')
+parser.add_argument('--test', action='store_true', help='Send 1/10 sampled emails to the sender for testing')
 
 args = parser.parse_args()
 
-# Mail's Subject
 subject = args.subject
 
 def get_data(csv_file):
@@ -40,7 +41,7 @@ def send_email(smtp_client, subject, sender, recipient, content):
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = recipient
-    body = MIMEText(content, 'plain')
+    body = MIMEText(content, 'html')
     msg.attach(body)
 
     smtp_client.sendmail(sender, recipient, msg.as_string())
@@ -49,10 +50,14 @@ if __name__ == "__main__":
     data = get_data(args.csv)
     template = load_template(args.template)
 
+    if args.test:
+        data = random.sample(data, max(1, len(data) // 10))
+
     with smtplib.SMTP(SMTP_SERVER, 587) as server:
         server.starttls()
         server.login(EMAIL, PASSWORD)
 
         for row in tqdm(data, desc="Sending emails"):
             content = template.format(**row)
-            send_email(server, subject, f"{IDENTIFICATION}<{EMAIL}>", row['email'], content)
+            recipient = EMAIL if args.test else row['email']
+            send_email(server, subject, f"{IDENTIFICATION}<{EMAIL}>", recipient, content)
